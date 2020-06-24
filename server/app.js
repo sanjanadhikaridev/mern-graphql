214a79e2849bd742619ab2b98b0e3dc37314f7bb
David <david@Davids-iMac.local>
2020-06-25 04:44:12 +0800

    update

--- server/app.js
+++ server/app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 
 const app = express();
+
+//Allow cross-origin requests
+app.use(cors());
 
 mongoose.connect('mongodb+srv://yuncong:UhjXNrPRwPuX5MP7@cluster0-zf9ju.mongodb.net/ninja-graphql?retryWrites=true&w=majority')
 mongoose.connection.once('open', () => {
