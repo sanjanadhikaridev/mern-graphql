239c408b323f774e1c8a146f69c5c7270a0a2d84
David <david@Davids-iMac.local>
2020-06-25 09:56:27 +0800

    update

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react'
-import { gql } from 'apollo-boost';
 import { graphql } from 'react-apollo';
+import { getAuthorsQuery } from '../queries/queries'
 
-const getAuthorsQuery = gql`
-    {
-        authors{
-            name
-            id
-        }
-    }
-`
+
 
 class AddBook extends Component {
     diaplayAuthors() {
